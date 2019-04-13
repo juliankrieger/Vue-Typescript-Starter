69828f361c78a4908f2dc5da714997af80ff0862
JKrieger <jkrieger@htwsaar.de>
2019-04-13 14:29:08 +0200

    add vue component example

---
+++ example/VueComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>This is a Vue Component</p>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import {CreateElement} from "vue";
+
+@Component
+export default class HelloWorld extends Vue {
+}
+</script>
